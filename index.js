3cb4833db540b731fd9eadaa3db3647688d74ea3
Hassan Shaikley <hassan.shaikley@gmail.com>
2016-12-05 14:54:04 -0500

    Converted simple file :D

--- index.js
+++ index.js
@@ -5,122 +5,106 @@ var write = require('fs-writefile-promise')
 const execPromise = require('child-process-promise').exec;
 
 // var markdown = '# title paragraph\nParagraph stuff\n\nA new line with an image ![Image](http://commonmark.org/help/images/favicon.png)';
-var markdown = '# Title\n\n# Another Title';
-
+var markdown = '# Hello ![GitHub Logo](/images/logo.png)';
 var docJSON = defaultMarkdownParser.parse(markdown)
 
 
-console.log(JSON.stringify(docJSON.toJSON()));
-var outputFilePath = 'pandocAST.json'
+let parentNodes = []; // stack for keeping track of the last node : )
+let outputParentNodes = []; // stack for keeping track of the last output node
+// let outputParentNode = null;
 
-// var count = 0;
+let blocks = []; // blocks is eventually set to this array.
+
+var outputFilePath = 'pandocAST-Attempt.json'
+
 // Initialize the output file
-var pandocJSON = {
-	"blocks": [
-
-	],
-	"pandoc-api-version":[
-		1,
-		17,
-		0,
-		4
-	],
-	"meta":{
-		// "converted-from-docjson": "true"
-	}
-}
+var pandocJSON = {}
 
 function traverse(){
 	console.log(`Traversing docJSON`)
-	console.log(docJSON)
+	console.log(JSON.stringify(docJSON.toJSON()));
 
 
-	function scanFragment( fragment, position, depth) {
-		fragment.forEach((child, offset) => scan(child, position + offset, depth))
+	function scanFragment( fragment, position) {
+		parentNodes.push(fragment);
+		if (fragment.content){
+			fragment.content.forEach((child, offset) => scan(child, position + offset))
+			outputParentNodes.pop();
+
+		}
+
 	}
-	function scan(node, position, depth) {
-		depth++;
-		console.log(`\nnodes at depth  ${depth}`)
-		console.log(node)
-		console.log('\n\n')
+	function scan(node, position) {
 
-		if (node.type.name === 'Heading'){
+		let newNode = {};
+
+		if (node.type === 'heading'){
+
+			let level = node.attrs.level;
 			newNode.t = "Header";
 			newNode.c = [];
 			newNode.c[0] = level;
 			newNode.c[1] = ["",[],[]]; // Don't fully understand this lol
 			newNode.c[2] = [];
+			// blocks.push(newNode);
+			createNode(newNode);
+			blocks.push(newNode);
+			outputParentNodes.push(newNode);
+
 		}
 
-		if ( node.isText ) {
-			console.log('looking at textnode')
-			console.log(node)
-		}
-		scanFragment(node.content, position + 1, depth)
-	}
-	scanFragment(docJSON, 0, 0)
-}
+		let newNodes = [];
+		// Text becomes an array of nodes
+		if (node.type === 'text'){
 
-function start(){
-
-	// console.log('docJSON:' + docJSON + '\n\n' + docJSON.content)
-	const content = docJSON.content.content;
-	console.log(content)
-	console.log(content.length)
-
-
-	for (let i = 0; i < content.length; i++ ){
-		console.log(`visiting node ${i}, have ${content[i]}`)
-		visit(content[i]);
-	}
-	// Need to traverse docJSON.content
-	// pandocJSON.blocks.c.push({..lol})
-	finish();
-
-}
-
-function visit(node){
-	console.log(`\n\nvisit`)
-	console.log(node)
-	console.log(node.type.name)
-	console.log(node.content)
-	let newNode = {};
-	switch(node.type.name){
-		case "heading":
-		console.log('yep heading')
-		let level = node.attrs.level;
-		newNode.t = "Header";
-		newNode.c = [];
-		newNode.c[0] = level;
-		newNode.c[1] = ["",[],[]]; // Don't fully understand this lol
-		newNode.c[2] = [];
-
-		console.log(node.content)
-		console.log(node.content.content)
-		console.log(node.content.content[0].text)
-
-
-		// Every word needs to be space separated in pandocAST
-		let words =  node.content.content[0].text.split(" ");
-		for (let i = 0; i < words.length; i++){
-			newNode.c[2].push({t: "Str", c: words[i]})
-
-			//if not at last word add a space
-			if (i < words.length -1){
-				newNode.c[2].push({t: "Space"})
+			// let words =  node.text.split(" ");
+			newNodes = generateTextNodes(node.text);
+			// blocks.push(newNodes); // ????? Nooo
+			// outputParentNodes.push(newNodes); ?? I think text is a leaf so don't have to push
+			for (var i in newNodes) {
+				createNode(newNodes[i])
 			}
 		}
 
-		console.log('\n\n PUSHING NEW NODE '+JSON.stringify(newNode))
+		if (node.type === 'image'){
+			console.log('an image')
+			newNode.t = "Image";
+			newNode.c = [
 
-		pandocJSON.blocks.push(newNode);
+			];
+			newNode.c[0] = ["",[
 
-		break;
+			],[
+
+			]]; // Don't fully understand this lol
+			newNode.c[1] = generateTextNodes(node.attrs.alt)
+			newNode.c[2] = [node.attrs.src, ""];
+			// blocks.push(newNode); // ????? Nooo
+			createNode(newNode)
+			// outputParentNodes.push(newNode);
+		}
+
+
+
+		scanFragment(node, position + 1)
+		parentNodes.pop();
 	}
+	scanFragment(docJSON.toJSON(), 0)
+	pandocJSON.blocks = blocks;
+	pandocJSON["pandoc-api-version"] = [
+			1,
+			17,
+			0,
+			4
+		];
+		pandocJSON.meta = {};
+
+	finish();
 }
 
 
 function finish(){
+	console.log('finish called')
 	return write(outputFilePath, JSON.stringify(pandocJSON, null, "\t"))
 	.then(function(fn){
 		console.log('written')
@@ -128,12 +112,36 @@ function finish(){
 	})
 	.then(function(idk){
 		console.log(`done converting`)
-		// console.log(`return is ${JSON.stringify(idk)}`)
 	})
 	.catch((error)=>{
 		console.log("crap an erorr")
 	})
 }
 
-// start();
+function generateTextNodes(words){
+	let newNodes = [];
+	words = words.split(" ")
+	console.log('words is ' + words)
+	for (let i = 0; i < words.length; i++){
+		if (words[i] == "") continue;
+		newNodes.push({t: "Str", c: words[i]})
+		if (i < words.length - 1){
+			newNodes.push({t: "Space"})
+		}
+	}
+	return newNodes;
+}
+
+function createNode(newNode){
+	console.log(`blocks is ${JSON.stringify(blocks)}`)
+	var parent = outputParentNodes[outputParentNodes.length-1];
+	console.log(`parent is ${JSON.stringify(parent)}`)
+	if (parent){
+		parent.c[2].push(newNode);
+	} else {
+		outputParentNodes.push(newNode)
+	}
+}
+
+
 traverse();
