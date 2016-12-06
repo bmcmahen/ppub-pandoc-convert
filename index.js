59e15fc670bb71a022bfcb74e42f4225df350098
Hassan Shaikley <hassan.shaikley@gmail.com>
2016-12-06 11:28:15 -0500

    Refactored + cleaned up

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var execPromise = require('child-process-promise').exec;
 
 var outputFilePath = 'pandocAST-Attempt.json';
 var markdown = '*Italicss* \n\n**Boldss**';
-var parentNodes = []; // stack for keeping track of the last node : )
-var outputParentNodes = []; // stack for keeping track of the last output node
+var currentParentNodes = []; // stack for keeping track of the last node : )
+var currentOutputParentNodes = []; // stack for keeping track of the last output node
 var blocks = []; // blocks is eventually set to this array.
 
 var pandocJSON = {};
@@ -23,7 +23,7 @@ function buildPandocAST(){
 	cyan(JSON.stringify(docJSON.toJSON()));
 
 	function scanFragment( fragment, position) {
-		parentNodes.push(fragment);
+		currentParentNodes.push(fragment);
 		if (fragment.content){
 			fragment.content.forEach((child, offset) => scan(child, position + offset));
 		}
@@ -32,7 +32,7 @@ function buildPandocAST(){
 	// Create a node
 	// If node is a root node, push it to blocks
 	function scan(node, position) {
-		green(`\nBlocks:\t${JSON.stringify(blocks)}\nParentNodes:\t${JSON.stringify(parentNodes)}\nOutputParentNodes:\t${JSON.stringify(outputParentNodes)}\n`)
+		green(`\nBlocks:\t${JSON.stringify(blocks)}\nParentNodes:\t${JSON.stringify(currentParentNodes)}\nOutputParentNodes:\t${JSON.stringify(currentOutputParentNodes)}\n`)
 		let newNode = {};
 		let markCount = 0;
 
@@ -44,7 +44,7 @@ function buildPandocAST(){
 				newNode.c[0] = level;
 				newNode.c[1] = ["",[],[]]; // Don't fully understand this lol
 				newNode.c[2] = [];
-				createNode(newNode);
+				addNode(newNode);
 				blocks.push(newNode);
 				break;
 			case "text":
@@ -55,14 +55,14 @@ function buildPandocAST(){
 							newerNode = {};
 							newerNode.t = "Emph";
 							newerNode.c = [];
-							createNode(newerNode);
+							addNode(newerNode);
 							markCount++;
 						}
 						if (node.marks[i].type === 'strong'){
 							newerNode = {};
 							newerNode.t = "Strong";
 							newerNode.c = [];
-							createNode(newerNode);
+							addNode(newerNode);
 							markCount++;
 						}
 					}
@@ -70,26 +70,26 @@ function buildPandocAST(){
 
 				let newNodes = [];
 				// let words =  node.text.split(" ");
-				newNodes = createTextLeaves(node.text);
+				newNodes = createTextNodes(node.text);
 				// blocks.push(newNodes); // ????? Nooo
-				// outputParentNodes.push(newNodes); ?? I think text is a leaf so don't have to push
+				// currentOutputParentNodes.push(newNodes); ?? I think text is a leaf so don't have to push
 				for (var i in newNodes) {
-					createNode(newNodes[i])
+					addNode(newNodes[i])
 				}
 				break;
 			case "image":
 				newNode.t = "Image";
 				newNode.c = [];
 				newNode.c[0] = ["",[],[]]; // Don't fully understand this lol
-				newNode.c[1] = createTextLeaves(node.attrs.alt)
+				newNode.c[1] = createTextNodes(node.attrs.alt)
 				newNode.c[2] = [node.attrs.src, ""];
-				createNode(newNode)
+				addNode(newNode)
 				break;
 			case "paragraph":
 				newNode.t = "Para";
 				newNode.c = [];
 
-				createNode(newNode);
+				addNode(newNode);
 				blocks.push(newNode);
 				break;
 			default:
@@ -103,15 +103,15 @@ function buildPandocAST(){
 		while (markCount > 0){
 			markCount--;
 			blue('popping output: Mark')
-			outputParentNodes.pop();
-			parentNodes.pop(); // Why do this?? Do you need to do this bc I don't think so
+			currentOutputParentNodes.pop();
+			currentParentNodes.pop(); // Why do this?? Do you need to do this bc I don't think so
 			// ^^ Because these aren't parent Ndoes in docJSON
 		}
 
 		if (node.type === 'paragraph' || node.type === 'heading'){
 			blue('popping output: Paragraph or Heading')
-			outputParentNodes.pop();
-			parentNodes.pop();
+			currentOutputParentNodes.pop();
+			currentParentNodes.pop();
 		}
 	}
 	scanFragment(docJSON.toJSON(), 0)
@@ -129,7 +129,6 @@ function buildPandocAST(){
 
 
 function finish(){
-
 	return write(outputFilePath, JSON.stringify(pandocJSON, null, "\t"))
 	.then(function(fn){
 		console.log('written')
@@ -143,7 +142,7 @@ function finish(){
 	})
 }
 
-function createTextLeaves(words){
+function createTextNodes(words){
 	let newNodes = [];
 	words = words.trim().split(" ")
 	for (let i = 0; i < words.length; i++){
@@ -156,27 +155,27 @@ function createTextLeaves(words){
 	return newNodes;
 }
 
-function createNode(newNode){
+function addNode(newNode){
 	console.log(`blocks is ${JSON.stringify(blocks)}`)
 	console.log(newNode.t)
-	var parent = outputParentNodes[outputParentNodes.length-1];
+	var parent = currentOutputParentNodes[currentOutputParentNodes.length-1];
 	console.log(`parent is ${JSON.stringify(parent)}`)
 	if (parent){
-		yellow(`parent type is ${parent.t}, parent is ${JSON.stringify(parent)}, outerParentNodes is ${JSON.stringify(outputParentNodes)}`)
+		yellow(`parent type is ${parent.t}, parent is ${JSON.stringify(parent)}, outerParentNodes is ${JSON.stringify(currentOutputParentNodes)}`)
 		if (parent.t === "Para" || parent.t === "Emph" || parent.t === "Strong"){
 			parent.c.push(newNode)
-			blue(`1: pushing output to: \t${JSON.stringify(outputParentNodes)}`)
-			outputParentNodes.push(newNode)
+			blue(`1: pushing output to: \t${JSON.stringify(currentOutputParentNodes)}`)
+			currentOutputParentNodes.push(newNode)
 		} else {
 			parent.c[2].push(newNode);
 		}
 	}
 	else {
-		blue(`2: pushing output to: \t${JSON.stringify(outputParentNodes)}`)
-		outputParentNodes.push(newNode)
+		blue(`2: pushing output to: \t${JSON.stringify(currentOutputParentNodes)}`)
+		currentOutputParentNodes.push(newNode)
 	}
-	parent = outputParentNodes[outputParentNodes.length-1];
-	yellow(`paren NOW is ${JSON.stringify(parent)}`)
+	parent = currentOutputParentNodes[currentOutputParentNodes.length-1];
+	yellow(`parent is now ${JSON.stringify(parent)}`)
 }
 
 buildPandocAST();
