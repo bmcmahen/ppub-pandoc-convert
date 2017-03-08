769f61620d82598ef0db3103c3dddc1e842b2d6e
Hassan Shaikley <hassan.shaikley@gmail.com>
2017-03-08 15:20:47 -0500

    Reworked test

--- test/toPpub/test.js
+++ test/toPpub/test.js
@@ -18,7 +18,45 @@ describe('Convert Pandoc to Ppub', function() {
 
 		write(ppubFile, JSON.stringify(ppub, null, '\t'))
 		.then(() => {
-			convertPpubToPandoc(ppub);
+			return convertPpubToPandoc(ppub);
+		})
+		.then((newPandoc) => {
+			return write(newPandocFile, JSON.stringify(newPandoc, null, '\t'));
+		})
+		.then(() => {
+			expect(ppub).to.exist;
+		})
+		.then(done, done);
+	});
+
+	it('simple superscript', (done) => {
+		const testName = 'superscript';
+		const ppubFile = `${__dirname}/ppub/${testName}.json`;
+		const newPandocFile = `${__dirname}/newPandoc/${testName}.json`;
+		const ppub = convert(require(`./${testName}.json`))
+
+		write(ppubFile, JSON.stringify(ppub, null, '\t'))
+		.then(() => {
+			return convertPpubToPandoc(ppub);
+		})
+		.then((newPandoc) => {
+			return write(newPandocFile, JSON.stringify(newPandoc, null, '\t'));
+		})
+		.then(() => {
+			expect(ppub).to.exist;
+		})
+		.then(done, done);
+	});
+
+	it('simple subscript', (done) => {
+		const testName = 'subscript';
+		const ppubFile = `${__dirname}/ppub/${testName}.json`;
+		const newPandocFile = `${__dirname}/newPandoc/${testName}.json`;
+		const ppub = convert(require(`./${testName}.json`))
+
+		write(ppubFile, JSON.stringify(ppub, null, '\t'))
+		.then(() => {
+			return convertPpubToPandoc(ppub);
 		})
 		.then((newPandoc) => {
 			return write(newPandocFile, JSON.stringify(newPandoc, null, '\t'));
