cc71433bf77e7a47b3c5aa7b65ffff264b06f6ad
Hassan Shaikley <hassan.shaikley@gmail.com>
2017-03-10 14:31:20 -0500

    When missing cmd arg notifies

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var pandocToPpub = require('./pandocToPpub').pandocToPpub;
 var ppubToPandoc = require('./ppubToPandoc').ppubToPandoc;
 
-if (process.argv[2] === '--toPandoc') {
+if (!process.argv[3] && process.argv[2]) {
+	console.log(`Wrong usage, use like 'node index --toPandoc file.json'`)
+} else if (process.argv[2] === '--toPandoc') {
 	ppubToPandoc(require(`./${process.argv[3]}`));
 } else if (process.argv[2] === '--toPpub') {
 	pandocToPpub(require(`./${process.argv[3]}`));
